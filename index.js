dc8a07f82c1dc1d3cea5f1ae6ac539f63ec9a741
marcohpk <hmarcopk@gmail.com>
2022-10-03 14:13:16 -0500

    headers to allow corcs

--- index.js
+++ index.js
@@ -8,7 +8,26 @@ const app = express();
 
 
 //Allow Corcs
-app.use(cors({origin: '*'}));
+app.use(cors({origin: 'https://angular-barber-shop.vercel.app'}));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://angular-barber-shop.vercel.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 
 // capturar body
