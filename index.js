94dc966d823a65e5acc94f1e031d32168adc64c1
marcohpk <hmarcopk@gmail.com>
2022-10-03 15:04:11 -0500

    corcs2 vercel

--- index.js
+++ index.js
@@ -8,13 +8,8 @@ const app = express();
 
 
 //Allow Corcs
-app.use(cors({origin: '*'}));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+const corcs = require('./corcs');
+app.use(corcs);
 
 // capturar body
 app.use(bodyparser.urlencoded({ extended: false }));
